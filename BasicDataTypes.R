bebf2992f0a2d50a72a69106a1ae3560c86bce2a
csk2023 <153008979+csk2023@users.noreply.github.com>
2024-05-15 22:51:25 +0530

    Create BasicDataTypes.R

---
+++ BasicDataTypes.R
@@ -0,0 +1,35 @@
+#Numeric Data Type
+x<-10
+y<-3.14
+print(c(x,y))
+
+#Integer Data Type
+a<-10L
+b<-as.integer(20)
+print(c(a,b))
+
+#Logical Data Type
+is_true<-TRUE
+is_false<-FALSE
+print(is_true)
+print(is_false)
+
+#Character Data Type
+name<-"John"
+message<-"Hello World"
+print(name)
+print(message)
+
+#Complex Data Type 
+c1<-complex(real=3,imaginary=2)
+c2<-2+3i
+print(c1)
+print(c2)
+
+#Raw Data Type 
+raw_data<-as.raw(c(0x48,0x65,0x6C,0x6C,0x6F))
+print(raw_data)
+
+
+raw_data<-as.raw(c(2x48,0x65,0x6C,0x6C,0x6F))
+print(raw_data)
