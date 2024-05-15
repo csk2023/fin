6ef52c4b91ca40ee6944cb1b443edba2b243a130
csk2023 <153008979+csk2023@users.noreply.github.com>
2024-05-15 22:48:48 +0530

    Update and rename one.R to BasicDataTypes.R

--- 1/BasicDataTypes.R
+++ 1/BasicDataTypes.R
@@ -1,32 +1,32 @@
-#A
+#Numeric Data Type
 x<-10
 y<-3.14
 print(c(x,y))
 
-#B
+#Integer Data Type
 a<-10L
 b<-as.integer(20)
 print(c(a,b))
 
-#C
+#Logical Data Type
 is_true<-TRUE
 is_false<-FALSE
 print(is_true)
 print(is_false)
 
-#D
+#Character Data Type
 name<-"John"
 message<-"Hello World"
 print(name)
 print(message)
 
-#E
+#Complex Data Type 
 c1<-complex(real=3,imaginary=2)
 c2<-2+3i
 print(c1)
 print(c2)
 
-#F
+#Raw Data Type 
 raw_data<-as.raw(c(0x48,0x65,0x6C,0x6C,0x6F))
 print(raw_data)
 
